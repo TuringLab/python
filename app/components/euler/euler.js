3331a6656b6110b248aabac0bee31709b5211fe4
Henry Miskin <henry.miskin@talktalk.net>
2015-06-30 17:30:10 +0100

    Added warnings about code progress and links to euler answers

--- app/components/euler/euler.js
+++ app/components/euler/euler.js
@@ -6,5 +6,8 @@ function EulerController($scope,$sce,$routeParams){
     var eulerUrl = 'http://projecteuler.net/problem=' + $scope.eulerId
     $scope.eulerUrl = $sce.trustAsResourceUrl(eulerUrl)
 
+    var eulerAnswerUrl = 'http://blog.dreamshire.com/project-euler-' + $scope.eulerId + '-solution/'
+    $scope.eulerAnswerUrl = $sce.trustAsResourceUrl(eulerAnswerUrl)
+
     $scope.answers = {1: '233168', 2: '4613732', 3: '6857', 4: '906609', 5: '232792560', 6: '25164150', 7: '104743', 8: '23514624000', 9: '31875000'};
 }
